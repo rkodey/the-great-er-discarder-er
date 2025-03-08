d8bd5568cf07131db10e08e956ecf6709c685990
Rob Kodey <rkodey+github@gmail.com>
2025-03-08 18:48:22 -0500

    Add optional Tab Groups on the Profiler page
    This includes optional permissions for Chrome, which is really what I was learning here!

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@
 
 # Change Log
 
+## 1.1.1 - ( In Progress )
+- New: Tab Groups are now ( optionally ) displayed on the Profiler page.
+
 ## 1.1.0 - Feb 2025
 - New: Migrate suspended tabs from several other extensions, like "The Marvellous Suspender" and "The Great Suspender (notrack)"
 - New: Automatic dark mode
@@ -25,7 +28,7 @@
 ## 0.2.1 - Oct 2021
 - Add new popup command to discard all eligible tabs based on options (aka no force) ( Closes upstream #18 )
 - Discard at startup option has its own group and visibility ( ( [Closes #9](https://github.com/rkodey/the-great-er-discarder-er/issues/9) ) - thanks @LordXerus )
-- Add option to enable links to the Chrome Discards page ( Closes upstream #39 )
+- Add option to enable links to the browser Discards page ( Closes upstream #39 )
 - Update options tab to switch to an existing tab instead of always opening a new tab
 - Update HTML to be a bit easier to read with heavier font and tweaked layout
 - Standardize and format HTML and CSS files
--- src/js/profiler.js
+++ src/js/profiler.js
@@ -1,21 +1,22 @@
-/* global chrome */
 
 (function () {
 
   'use strict';
 
   function generateTabInfo(table, info, first) {
+
     const
-      windowId = info?.windowId ?? '?',
-      tabId = info?.tabId ?? '?',
-      groupId = info?.groupId > 0 ? info.groupId : '',
-      // @INFO Not enabling tabGroups yet since it requires a new permission in the Manifest
-      // groupName = info?.group?.title ?? '',
-      // groupColor = info?.group?.color ?? '',
-      pinned = info?.pinned ? '<i class="fa fa-thumb-tack"></i>' : '',
-      tabTitle = info?.tab.title ?? 'unknown',
-      tabTimer = info?.timerUp ?? -1,
-      tabStatus = info?.status ?? 'unknown';
+      windowId    = info?.windowId      ?? '?',
+      tabId       = info?.tabId         ?? '?',
+      groupId     = info?.groupId > 0   ? info?.groupId : '',
+      groupName   = info?.group?.title,
+      groupColor  = info?.group?.color  ?? '',
+      pinned      = info?.pinned        ? '<i class="fa fa-thumb-tack"></i>' : '',
+      tabTitle    = info?.tab.title     ?? 'unknown',
+      tabStatus   = info?.status        ?? 'unknown',
+      tabTimer    = info?.timerUp       ?? '';
+
+    // console.log('5 tab info', groupId, groupName, groupColor);
 
     const row = table.insertRow();
     if (first) {
@@ -25,62 +26,93 @@
     row.insertCell().innerText = tabId;
 
     const group       = row.insertCell();
-    // group.style.color = groupColor;
-    // group.innerText   = groupName;
-    group.innerText   = groupId;
+    group.className   = 'center';
+    group.innerHTML   = groupName ? `<span class="group ${groupColor}">${groupName}</span>` : groupId;
 
-    const cell        = row.insertCell();
-    cell.className    = 'title';
-    cell.innerText    = tabTitle;
+    const title       = row.insertCell();
+    title.className   = 'title';
+    title.innerText   = tabTitle;
 
-    row.insertCell().innerText = tabTimer;
+    const timer       = row.insertCell();
+    timer.className   = 'center';
+    timer.innerText   = tabTimer;
+
     row.insertCell().innerText = tabStatus;
     row.insertCell().innerHTML = pinned;
   }
 
-  async function fetchInfo() {
-    // chrome.tabGroups.query({}, async function(groups) {
-
-      // const groupMap = {};
-      // groups.forEach((group) => {
-      //   groupMap[group.id] = group
-      // });
-
-      const windows = new Map();
-      const tabs = await chrome.tabs.query({});
-      for (let i = 0; i < tabs.length; ++i) {
-        const curTab = tabs[i];
-        if (!windows.has(curTab.windowId)) {
-          windows.set(curTab.windowId, []);
-        }
-        const win = windows.get(curTab.windowId);
-
-        const discardInfo = await chrome.runtime.sendMessage({ action: 'requestTabInfo', tab: curTab });
-        discardInfo.tab = curTab;
-        // discardInfo.group = groupMap[discardInfo.groupId];
-        win.push(discardInfo);
+  async function generateRows(groupMap) {
+    // console.log('4 main loop', groupMap);
+    const windows = new Map();
+    const tabs = await chrome.tabs.query({});
+    for (let i = 0; i < tabs.length; ++i) {
+      const curTab = tabs[i];
+      if (!windows.has(curTab.windowId)) {
+        windows.set(curTab.windowId, []);
       }
+      const win = windows.get(curTab.windowId);
 
-      const tableEl = document.getElementById('gsProfilerBody');
-      for (const winId of Array.from(windows.keys()).sort()) {
-        const infos = windows.get(winId);
-        let first = true;
-        for (const discardInfo of infos) {
-          generateTabInfo(tableEl, discardInfo, first);
-          first = false;
-        }
+      const discardInfo = await chrome.runtime.sendMessage({ action: 'requestTabInfo', tab: curTab });
+      discardInfo.tab = curTab;
+      if (groupMap) {
+        discardInfo.group = groupMap[discardInfo.groupId];
       }
-    // });
+      win.push(discardInfo);
+    }
+
+    const tableEl = document.getElementById('gsProfilerBody');
+    tableEl.innerHTML = '';
+
+    for (const winId of Array.from(windows.keys()).sort()) {
+      const infos = windows.get(winId);
+      let first = true;
+      for (const discardInfo of infos) {
+        generateTabInfo(tableEl, discardInfo, first);
+        first = false;
+      }
+    }
   }
 
-  window.onload = function () {
-    fetchInfo();
+  function generateTable() {
 
-    //handler for refresh
+    // console.log('1 perms');
+    chrome.permissions.contains({ permissions: ['tabGroups'] }, async (fShopGroups) => {
+
+      if (fShopGroups) {
+
+        document.getElementById('enableTabGroups').style.display = 'none';
+        // console.log('2 tabGroups', chrome.tabGroups);
+        const groupMap = {};
+        chrome.tabGroups?.query({}, function(groups) {
+          groups.forEach((group) => {
+            groupMap[group.id] = group;
+          });
+          generateRows(groupMap);
+        });
+        // console.log('3 tabGroups', chrome.tabGroups);
+
+      }
+      else {
+        generateRows();
+      }
+
+    });
+  }
+
+
+  window.onload = function () {
+
     document.getElementById('refreshProfiler').onclick = function (e) {
-      document.getElementById('gsProfilerBody').innerHTML = '';
-      fetchInfo();
+      generateTable();
     };
+
+    document.getElementById('enableTabGroups').onclick = function (e) {
+      chrome.permissions.request({ permissions: ['tabGroups'] }, (fAccepted) => {
+        generateTable();
+      });
+    };
+
+    generateTable();
 
     /*
     chrome.processes.onUpdatedWithMemory.addListener(function (processes) {
