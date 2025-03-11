9e2ca5dfddff6a0cd87d33ed01c5ac3f0c0aea7a
Rob Kodey <rkodey+github@gmail.com>
2025-03-10 21:00:39 -0400

    Update grunt setup
    Add a version banner to the combined background.js
    Add a build-watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,69 @@
+
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      manifest: grunt.file.readJSON('src/manifest.json'),
-      concat: {
-        options: {},
-        dist: {
-          src: [ "src/js/db.js", "src/js/storage.js", "src/js/tabStates.js", "src/js/eventPage.js" ],
-          dest: "src/js/background.js",
-        },
-      },
-      watch: {
-        scripts: {
-          files: [ "src/js/db.js", "src/js/storage.js", "src/js/tabStates.js", "src/js/eventPage.js" ],
-          tasks: ['concat'],
-          options: { spawn: false },
-        },
-      },
-      crx: {
-        myPublicExtension: {
-          src: [
-              "src/**/*",
-              "!src/js/db.js",
-              "!src/js/storage.js",
-              "!src/js/tabStates.js",
-              "!src/js/eventPage.js",
-              "!**/screenshot*.png",
-              "!**/Thumbs.db"
-          ],
-          dest: "build/zip/<%= pkg.name %>.zip",
-        },
+  const sources = [ "src/js/db.js", "src/js/storage.js", "src/js/tabStates.js", "src/js/eventPage.js" ];
 
-        mySignedExtension: {
-          src: [
-              "src/**/*",
-              "!src/js/db.js",
-              "!src/js/storage.js",
-              "!src/js/tabStates.js",
-              "!src/js/eventPage.js",
-              "!**/screenshot*.png",
-              "!**/Thumbs.db"
-          ],
-          dest: "build/crx/<%= pkg.name %>.crx",
-          options: {
-            privateKey: "key.pem"
-          }
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+    manifest: grunt.file.readJSON('src/manifest.json'),
+
+    concat: {
+      options: {
+        banner:
+          // '// @ts-check\n' +
+          '// <%= manifest.name %> - v<%= manifest.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n\n'
+      },
+      dist: {
+        src: sources,
+        dest: 'src/js/background.js',
+      },
+    },
+
+    watch: {
+      scripts: {
+        files: [ ...sources, 'Gruntfile.js' ],
+        tasks: [ 'concat' ],
+        options: { spawn: false },
+      },
+    },
+
+    crx: {
+      myPublicExtension: {
+        src: [
+            "src/**/*",
+            "!src/js/db.js",
+            "!src/js/storage.js",
+            "!src/js/tabStates.js",
+            "!src/js/eventPage.js",
+            "!**/screenshot*.png",
+            "!**/Thumbs.db"
+        ],
+        dest: "build/zip/<%= pkg.name %>.zip",
+      },
+
+      mySignedExtension: {
+        src: [
+            "src/**/*",
+            "!src/js/db.js",
+            "!src/js/storage.js",
+            "!src/js/tabStates.js",
+            "!src/js/eventPage.js",
+            "!**/screenshot*.png",
+            "!**/Thumbs.db"
+        ],
+        dest: "build/crx/<%= pkg.name %>.crx",
+        options: {
+          privateKey: "key.pem"
         }
       }
-    });
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-crx');
-    grunt.registerTask('default', ['concat', 'crx']);
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-crx');
+  grunt.registerTask('default', ['concat', 'crx']);
+  grunt.registerTask('build-watch', ['concat', 'watch']);
+
 };
